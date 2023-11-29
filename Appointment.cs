65b43244f78f95d9664d50c97be8b4363fb6bc09
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-11-29 14:47:49 +0100

    Med Appointment-klasse

---
+++ Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VindegadeKS_WPF
+{
+    public class Appointment
+    {
+        public int ApmtId { get; set; }
+        public DateTime ApmtDate { get; set; }
+
+        public Appointment(int _apmtId, DateTime _apmtDate)
+        {
+            ApmtId = _apmtId;
+            ApmtDate = _apmtDate;
+        }
+
+        public Appointment() : this(0, default)
+        { }
+    }
+}
