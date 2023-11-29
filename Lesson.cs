881014e0c3efdc0707902fb96305b0110ffd65a7
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-11-29 14:47:49 +0100

    Med Appointment-klasse

--- Lesson.cs
+++ Lesson.cs
@@ -11,18 +11,16 @@ namespace VindegadeKS_WPF
         public int LesId { get; set; }
         public string LesName { get; set; }
         public string LesType { get; set; }
-        public DateOnly LesDate { get; set; }
         public string LesDescription { get; set; }
 
-        public Lesson(int _lesId, string _lesName, string _lesType, DateOnly _lesDate, string _lesDescription)
+        public Lesson(int _lesId, string _lesName, string _lesType, string _lesDescription)
         {
             LesId = _lesId;
             LesName = _lesName;
             LesType = _lesType;
-            LesDate = _lesDate;
             LesDescription = _lesDescription;
         }
-        public Lesson() : this(0, "", "", default, "")
+        public Lesson() : this(0, "", "", "")
         { }
     }
 }
