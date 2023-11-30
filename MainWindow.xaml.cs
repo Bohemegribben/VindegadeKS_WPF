7f4b00a42ac4e4884f9fe361b6936486d9355310
Allan Buhl Blindbæk <abbl52712@edu.ucl.dk>
2023-11-30 13:17:47 +0100

    Med basis-GUI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,75 @@ namespace VindegadeKS_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        //samtlige pages initialiseres inden MainWindow-metoden køres for at undgå reset af pages under navigation 
+        DashboardPage dP = new DashboardPage();
+        AppointmentPage aP = new AppointmentPage();
+        ClassPage cP = new ClassPage();
+        InstructorPage iP = new InstructorPage();
+        LessonPage lP = new LessonPage();
+        StudentPage sP = new StudentPage();
+
         public MainWindow()
         {
             InitializeComponent();
+            Menu_Dash_Button.Background = Brushes.Gray; //Dashboardknappen gøres grå for at indikere, hvor brugeren 'befinder' sig
+            PageView.Content = dP; //startsiden kaldes
+        }
+
+        //De nedenstående Button_Click-knapper bruges til at navigere mellem de primærsiderne (svarende til klasserne).
+        //Dashboardet er startside, de andre er undersider derfra.
+        private void Menu_Dash_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons(); //alle knapper gøres lysegrå
+            PageView.Content = dP;
+            Menu_Dash_Button.Background = Brushes.Gray; //knappen gøres grå, når der trykkes på den
+        }
+
+        private void Menu_Class_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons();
+            PageView.Content = cP;
+            Menu_Class_Button.Background = Brushes.Gray;
+        }
+
+        private void Menu_Stu_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons();
+            PageView.Content = sP;
+            Menu_Stu_Button.Background = Brushes.Gray;
+        }
+
+        private void Menu_Inst_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons();
+            PageView.Content = iP;
+            Menu_Inst_Button.Background = Brushes.Gray;
+        }
+
+        private void Menu_Apmt_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons();
+            PageView.Content = aP;
+            Menu_Apmt_Button.Background = Brushes.Gray;
+        }
+
+        private void Menu_Les_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LightGrayButtons();
+            PageView.Content = lP;
+            Menu_Les_Button.Background = Brushes.Gray;
+        }
+
+        //LightGrayButtons: Metode der gør alle knapper lysegrå.
+        //Den kaldes som det første, når der trykke på en ny knap
+        private void LightGrayButtons() 
+        {
+            Menu_Dash_Button.Background = Brushes.LightGray;
+            Menu_Class_Button.Background = Brushes.LightGray;
+            Menu_Stu_Button.Background = Brushes.LightGray;
+            Menu_Inst_Button.Background = Brushes.LightGray;
+            Menu_Les_Button.Background = Brushes.LightGray;
+            Menu_Apmt_Button.Background = Brushes.LightGray;
         }
     }
 }
