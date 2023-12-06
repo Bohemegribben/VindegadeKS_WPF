a3dff87e0fe4066e3a8d160667e40bd612e2d3e1
carolinanoemi <cnoemimartinez@gmail.com>
2023-12-06 19:39:37 +0100

    StudentPage Save button SQL con

--- StudentPage.xaml.cs
+++ StudentPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography.X509Certificates;
 
 namespace VindegadeKS_WPF
 {
@@ -25,6 +29,7 @@ namespace VindegadeKS_WPF
             InitializeComponent();
             this.DataContext = this; // Binder denne klasse til dens XAML view
             LoadStudentsList(); // Indlæser listen af studerende ved opstart
+            LockInputFields();
         }
 
         private void LoadStudentsList() 
@@ -33,6 +38,43 @@ namespace VindegadeKS_WPF
 
 
         }
+
+        #region Lock and Unlock inputFields
+
+        // metode til at låse textboxe/input boxe fra start
+        private void LockInputFields()
+        {
+            stu_txtBox_cpr.IsEnabled = false;
+            stu_txtBox_fornavn.IsEnabled = false;
+            stu_txtBox_efternavn.IsEnabled = false;
+            stu_txtBox_telefon.IsEnabled = false;
+            stu_txtBox_email.IsEnabled = false;
+        }
+
+        // //Unlocks all inputfields, so that they can be edited
+        private void UnlockInputFields()
+        {
+            stu_txtBox_cpr.IsEnabled = true;
+            stu_txtBox_fornavn.IsEnabled = true;
+            stu_txtBox_efternavn.IsEnabled = true;
+            stu_txtBox_telefon.IsEnabled = true;
+            stu_txtBox_email.IsEnabled = true;
+        }
+
+        #endregion
+
+        #region clear input
+
+        private void ClearInput() 
+        {
+            stu_txtBox_cpr.Clear();
+            stu_txtBox_fornavn.Clear();
+            stu_txtBox_efternavn.Clear();
+            stu_txtBox_telefon.Clear();
+            stu_txtBox_email.Clear();
+        }
+
+        #endregion
 
         #region Search_Bar
         private void watermarkTxtBox_GotFocus(object sender, RoutedEventArgs e)
@@ -43,15 +85,15 @@ namespace VindegadeKS_WPF
             // og vores "search textbox skal være synlig
             stu_txtBox_search.Visibility = Visibility.Visible;
             stu_txtBox_search.Focus();
-        
+
         }
 
-     
+
 
         private void searchTxtBox_LostFocus(object sender, RoutedEventArgs e)
         {
             // Hvis search bar er tom
-            if (string.IsNullOrEmpty(stu_txtBox_search.Text)) 
+            if (string.IsNullOrEmpty(stu_txtBox_search.Text))
             {
                 // så skal search textbox ikke være synlig
                 stu_txtBox_search.Visibility = Visibility.Collapsed;
@@ -67,11 +109,13 @@ namespace VindegadeKS_WPF
         #region Buttons
         private void Stu_Add_Button_Click(object sender, RoutedEventArgs e)
         {
+            UnlockInputFields();
 
         }
 
         private void Stu_Save_Button_Click(object sender, RoutedEventArgs e)
         {
+           
 
         }
 
@@ -86,7 +130,25 @@ namespace VindegadeKS_WPF
         }
         #endregion
 
-       
+        #region Button methods
+
+        public void SaveStudent (Student studentToBeCreated) 
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseServerInstance"].ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO VK_Instructors (InstFirstName, InstLastName, InstPhone, InstEmail)" +
+                                                 "VALUES(@InstFirstName,@InstLastName,@InstPhone,@InstEmail)" +
+                                                 "SELECT @@IDENTITY", con);
+                cmd.Parameters.Add("@InstFirstName", SqlDbType.NVarChar).Value = studentToBeCreated.StuCPR;
+                cmd.Parameters.Add("@InstLastName", SqlDbType.NVarChar).Value = studentToBeCreated.StuFirstName;
+                cmd.Parameters.Add("@InstPhone", SqlDbType.NVarChar).Value = studentToBeCreated.StuLastName;
+                cmd.Parameters.Add("@InstEmail", SqlDbType.NVarChar).Value = studentToBeCreated.StuPhone;
+                cmd.Parameters.Add("@InstEmail", SqlDbType.NVarChar).Value = studentToBeCreated.StuEmail;
+
+               
+            }
+        }
 
         private void studentsListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
